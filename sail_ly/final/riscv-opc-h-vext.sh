b294ebfcb33ea920b4959f1c10f987fa51adc254
nickcoding <50139127+nickyoyo@users.noreply.github.com>
2024-12-07 12:12:57 +0800

    Delete sail_ly directory

--- sail_ly/final/riscv-opc-h-vext.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 指定包含 .sail 文件的目录，这里假设是当前目录
-DIRECTORY="/root/sail-riscv/ocmaltest/sail_ly/final/sail-model-vext"
-
-# 找到所有 .sail 文件，并将它们的内容传递给 ./parser 程序
-find "$DIRECTORY" -type f -name '*.sail' -exec cat {} + | ./vextparser
--- sail_ly/riscv-opc-h.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 指定包含 .sail 檔案的目錄，這裡假設是目前目錄
-DIRECTORY="/root/sail-riscv/ocmaltest/sail_ly/sail-model"
-
-# 找到所有 .sail 文件，並將它們的內容傳遞給 ./parser 程式
-find "$DIRECTORY" -type f -name '*.sail' -exec cat {} + | ./parser
